5a097882a8b93fc25ce95fc714980a6c24a15913
manhtai <vo@manhtai.com>
2018-04-17 21:55:45 +0700

    UC 2.1

---
+++ chap02/uc21.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func uc21() {
+	cipher := "bsaspp kkuosp"
+	key := "rsidpy dkawoa"
+	text := ""
+	for i, c := range cipher {
+		x := int(c - 'a')
+		s := int(key[i] - 'a')
+		y := (x - s) % 26
+		if y < 0 {
+			y += 26
+		}
+		text += string('a' + y)
+	}
+	fmt.Print(text)
+}
