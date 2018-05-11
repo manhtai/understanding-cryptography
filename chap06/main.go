1fad03fa8de40427b2c15e2ef9cdaeb6f7612628
manhtai <vo@manhtai.com>
2018-05-11 23:31:26 +0700

    Done 6.5

---
+++ chap06/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	uc65()
+	// uc66()
+	// uc67()
+	// uc610()
+}
---
+++ chap06/uc65.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func gcd(m, n int) int {
+	if m < n {
+		return gcd(n, m)
+	}
+	if n == 0 {
+		return m
+	}
+	return gcd(m%n, n)
+}
+
+func gcdPrint(m, n int) {
+	fmt.Printf("gcd(%d, %d) = %d\n", m, n, gcd(m, n))
+}
+
+func uc65() {
+	gcdPrint(7469, 2464)
+	gcdPrint(2689, 4001)
+}
