cce85522a1af3f416b365a2c347b718f4bf08c23
manhtai <vo@manhtai.com>
2018-05-16 22:14:23 +0700

    Done chap 9

--- chap09/uc99.go
+++ chap09/uc99.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func uc99() {
 	fmt.Println("======= (9.9) =======")
+	ec := EC{a: 1, b: 6, p: 11}
+	a := 6
+	xB, yB := 5, 9
+	xAB, yAB := ec.dna(xB, yB, a)
+	fmt.Println(xAB, yAB)
 }
